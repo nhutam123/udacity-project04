68c7543bfbf378eed69fede03f60103b4802ccf8
nhu tam <soledad.guitarist@gmail.com>
2023-03-10 09:42:03 +0000

    add upload docker

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=my-project
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login -u tamln2
+docker tag my-project $dockerpath:lastest
 
 # Step 3:
 # Push image to a docker repository
+
+docker push $dockerpath:lastest
